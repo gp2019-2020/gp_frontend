621de9451a4dd8d1d909bb32a7260455d7e3fb7a
Miguel <gomesmike24@hotmail.com>
2020-06-22 14:07:07 +0100

    Last Update

--- GpFrontend/src/app/app.component.ts
+++ GpFrontend/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   page_name = 'Somos Voluntários';
   login = 'Login';
